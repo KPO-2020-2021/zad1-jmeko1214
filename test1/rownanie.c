70f07754d83f4229555a26e5d3c4a5c503b85471
Jan Merwiński <252959@studetn.pwr.edu.pl>
2021-03-04 09:40:48 +0100

    Test1-kompilacja

--- test1/rownanie.c
+++ test1/rownanie.c
@@ -41,7 +41,7 @@ int main() {
   
   if (Delta_2 > 0) {
 
-      Delta = sqrt( Delta_2 );
+    Delta = sqrt( Delta_2 ); /*przy kompilacji w C trzeba dodać '-lm', a przy kompilacji w C++ nie*/
       X1 = (-TKw.b - Delta) / (2*TKw.a);
       X2 = (-TKw.b + Delta) / (2*TKw.a);
       Ilosc_Pierwiastkow = 2;      
